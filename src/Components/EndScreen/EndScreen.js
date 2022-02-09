8c4a638eb76aa0a3a0dccc191a6d03fb9e2ef6d9
gichul <gichul0317@gmail.com>
2022-02-09 17:53:51 -0500

    endscreen updated

--- src/Components/EndScreen/EndScreen.js
+++ src/Components/EndScreen/EndScreen.js
@@ -1,21 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { UserContext } from '../../store/user-context';
 import styles from './EndScreen.module.css';
+import StartScreen from '../StartScreen/StartScreen';
 
 function EndScreen(props) {
+  const [playAgain, setPlayAgain] = useState(false);
+
   const ctx = useContext(UserContext);
 
   const userData = [...props.userData];
   const userList = userData.sort((a, b) =>
     a.cleartime > b.cleartime ? 1 : b.cleartime > a.cleartime ? -1 : 0
   );
-  console.log(userData);
-  console.log(userList);
 
-  return (
+  let content = (
     <div className={styles.end}>
       <div className={styles.user}>
-        <h1>{ctx.name}</h1>
+        <span>{ctx.name}, check your score</span>
       </div>
       <ul className={styles.userlist}>
         {userList.map((item) => {
@@ -27,9 +28,17 @@ function EndScreen(props) {
           );
         })}
       </ul>
-      <button>Play Again</button>
+      <div className={styles.btnbox}>
+        <button onClick={() => setPlayAgain(true)}>Play Again</button>
+      </div>
     </div>
   );
+
+  if (playAgain === true) {
+    content = <StartScreen />;
+  }
+
+  return content;
 }
 
 export default EndScreen;

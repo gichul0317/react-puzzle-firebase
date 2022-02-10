54def305d812c953af7a996e702b6a741b185aee
gichul <gichul0317@gmail.com>
2022-02-10 17:03:26 -0500

    deploy ready

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import StartScreen from './Components/StartScreen/StartScreen';
 import MobileScreen from './Components/MobileScreen/MobileScreen';
 
 function App() {
+  // state for usercontext
   const [userInfo, setUserInfo] = useState([]);
 
   const handleUserInfo = (info) => {
     setUserInfo(info);
   };
 
+  // calculate screen width and height
   const isDesktopWidth = useMatchMedia('(min-width:800px)', true);
   const isDesktopHeight = useMatchMedia('(min-height:800px)', true);
 
